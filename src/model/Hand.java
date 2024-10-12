9fd897f3aa025b6e3530053df556903d4323ddd4
Eduardo Zacour <eduardo.zacour@protonmail.com>
2024-10-12 15:32:26 -0300

    Created points update for class Hand

--- src/model/Hand.java
+++ src/model/Hand.java
@@ -1,5 +1,25 @@
 package model;
 
-class Hand {
+import java.util.ArrayList;
 
+class Hand {
+  public ArrayList<Card> cards = new ArrayList<>();
+  public int points = 0;
+
+  void updatePoints() {
+    int sum = 0;
+    boolean hasAce = false;
+    for (Card c : cards) {
+      if (c.rank == Rank.ACE) {
+        hasAce = true;
+      }
+      sum += c.getValue();
+    }
+
+    if (hasAce && sum < 12) {
+      sum += 10;
+    }
+
+    this.points = sum;
+  }
 }
