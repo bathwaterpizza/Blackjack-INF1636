e0a1523f176e640da70e9da2fd52d80047d9add6
Daniel Tannure <107507231+danieltannure@users.noreply.github.com>
2024-10-13 16:23:09 -0300

    Junit test for Card.class

--- test/model/CardTest.java
+++ test/model/CardTest.java
@@ -1,14 +1,61 @@
 package model;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.*;  
 
-import org.junit.Test;
+import org.junit.Test;  
 
 public class CardTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void TesteValorAs() {
+        Card carta = new Card(Suit.CLUBS, Rank.ACE);
+        assertEquals(1, carta.getValue());
+    }
 
+    @Test
+    public void TesteValor5() {
+        Card carta = new Card(Suit.DIAMONDS, Rank.FIVE);
+        assertEquals(5, carta.getValue());
+    }
+
+    @Test
+    public void TesteValor10() {
+        Card carta = new Card(Suit.HEARTS, Rank.TEN);
+        assertEquals(10, carta.getValue());
+    }
+
+    @Test
+    public void TesteValorRainha() {
+        Card carta = new Card(Suit.SPADES, Rank.QUEEN);
+        assertEquals(10, carta.getValue());
+    }
+
+    @Test
+    public void TesteValorValete() {
+        Card carta = new Card(Suit.SPADES, Rank.JACK);
+        assertEquals(10, carta.getValue());
+    }
+
+    @Test
+    public void TesteValorIgual() {
+        Card carta1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
+        Card carta2 = new Card(Suit.DIAMONDS, Rank.SEVEN);
+        assertTrue(carta1.equals(carta2));
+    }
+
+    @Test
+    public void TesteValorDiferenteNaipe() {
+        Card carta1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
+        Card carta2 = new Card(Suit.HEARTS, Rank.SEVEN);
+        assertFalse(carta1.equals(carta2));
+    }
+
+    @Test
+    public void TesteValorDiferenteValor() {
+        Card carta1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
+        Card carta2 = new Card(Suit.DIAMONDS, Rank.EIGHT);
+        assertFalse(carta1.equals(carta2));
+    }
 }
+
+
