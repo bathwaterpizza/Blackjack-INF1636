ed033577aab1c76a666d44bca46ca121cc33c978
bathwaterpizza <cakto002@gmail.com>
2024-10-13 20:52:02 -0300

    Document card tests

--- test/model/CardTest.java
+++ test/model/CardTest.java
@@ -5,36 +5,42 @@ import org.junit.Test;
 
 public class CardTest {
   @Test
+  // Test that the value of an Ace card is 1
   public void testValueAce() {
     Card card = new Card(Suit.CLUBS, Rank.ACE);
     assertEquals(1, card.getValue());
   }
 
   @Test
+  // Test that the value of a Five card is 5
   public void testValueFive() {
     Card card = new Card(Suit.DIAMONDS, Rank.FIVE);
     assertEquals(5, card.getValue());
   }
 
   @Test
+  // Test that the value of a Ten card is 10
   public void testValueTen() {
     Card card = new Card(Suit.HEARTS, Rank.TEN);
     assertEquals(10, card.getValue());
   }
 
   @Test
+  // Test that the value of a Queen card is 10
   public void testValueQueen() {
     Card card = new Card(Suit.SPADES, Rank.QUEEN);
     assertEquals(10, card.getValue());
   }
 
   @Test
+  // Test that the value of a Jack card is 10
   public void testValueJack() {
     Card card = new Card(Suit.SPADES, Rank.JACK);
     assertEquals(10, card.getValue());
   }
 
   @Test
+  // Test that two cards with the same suit and rank are equal
   public void testEqualValue() {
     Card card1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
     Card card2 = new Card(Suit.DIAMONDS, Rank.SEVEN);
@@ -42,12 +48,14 @@ public class CardTest {
   }
 
   @Test
+  // Test that a card is equal to itself
   public void testEqualItself() {
     Card card1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
     assertTrue(card1.equals(card1));
   }
 
   @Test
+  // Test that two cards with the same rank but different suits are not equal
   public void testDifferentSuit() {
     Card card1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
     Card card2 = new Card(Suit.HEARTS, Rank.SEVEN);
@@ -55,6 +63,7 @@ public class CardTest {
   }
 
   @Test
+  // Test that two cards with the same suit but different ranks are not equal
   public void testDifferentRank() {
     Card card1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
     Card card2 = new Card(Suit.DIAMONDS, Rank.EIGHT);
@@ -62,6 +71,7 @@ public class CardTest {
   }
 
   @Test
+  // Test that a card is not equal to null
   public void testDifferentNull() {
     Card card1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
     Card card2 = null;
@@ -69,10 +79,10 @@ public class CardTest {
   }
 
   @Test
+  // Test that a card is not equal to an object of a different class
   public void testDifferentClass() {
     Card card1 = new Card(Suit.DIAMONDS, Rank.SEVEN);
     Deck deck = new Deck(true);
     assertFalse(card1.equals(deck));
   }
-
 }
