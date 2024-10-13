a695c4b6204609696a2644303e54e7df3822186d
bathwaterpizza <cakto002@gmail.com>
2024-10-13 20:08:03 -0300

    Finished hand tests

--- test/model/HandTest.java
+++ test/model/HandTest.java
@@ -1,39 +1,105 @@
 package model;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HandTest {
+  private Hand hand;
+
+  @Before
+  public void setUp() {
+    hand = new Hand();
+  }
+
   @Test
   public void testAddCard() {
-    Hand hand = new Hand();
-    Card card = new Card(Suit.SPADES, Rank.ACE);
+    // Test adding a card to the hand and updating points
+    Card card = new Card(Suit.HEARTS, Rank.TEN);
     hand.addCard(card);
-    assertEquals("Hand points should be 11", 11, hand.points);
+    assertEquals(1, hand.cards.size());
+    assertEquals(10, hand.points);
+  }
+
+  @Test
+  public void testUpdatePointsWithAce() {
+    // Test updating points when an Ace is added, considering its value as 11
+    Card ace = new Card(Suit.SPADES, Rank.ACE);
+    Card nine = new Card(Suit.HEARTS, Rank.NINE);
+    hand.addCard(ace);
+    hand.addCard(nine);
+    assertEquals(20, hand.points);
+  }
+
+  @Test
+  public void testUpdatePointsWithoutAce() {
+    // Test updating points when no Ace is present
+    Card ten = new Card(Suit.HEARTS, Rank.TEN);
+    Card nine = new Card(Suit.CLUBS, Rank.NINE);
+    hand.addCard(ten);
+    hand.addCard(nine);
+    assertEquals(19, hand.points);
   }
 
   @Test
   public void testIsBlackjack() {
-    Hand hand = new Hand();
-    hand.addCard(new Card(Suit.SPADES, Rank.ACE));
-    hand.addCard(new Card(Suit.HEARTS, Rank.KING));
-    assertTrue("Hand should be a blackjack", hand.isBlackjack());
+    // Test if the hand is a blackjack (Ace and a 10-value card)
+    Card ace = new Card(Suit.SPADES, Rank.ACE);
+    Card ten = new Card(Suit.HEARTS, Rank.TEN);
+    hand.addCard(ace);
+    hand.addCard(ten);
+    assertTrue(hand.isBlackjack());
+  }
+
+  @Test
+  public void testIsNotBlackjack() {
+    // Test if the hand is not a blackjack
+    Card ace = new Card(Suit.SPADES, Rank.ACE);
+    Card nine = new Card(Suit.HEARTS, Rank.NINE);
+    hand.addCard(ace);
+    hand.addCard(nine);
+    assertFalse(hand.isBlackjack());
   }
 
   @Test
   public void testIsBust() {
-    Hand hand = new Hand();
-    hand.addCard(new Card(Suit.SPADES, Rank.TEN));
-    hand.addCard(new Card(Suit.HEARTS, Rank.TEN));
-    hand.addCard(new Card(Suit.CLUBS, Rank.TWO));
-    assertTrue("Hand should be bust", hand.isBust());
+    // Test if the hand is a bust (points exceed 21)
+    Card ten = new Card(Suit.HEARTS, Rank.TEN);
+    Card nine = new Card(Suit.CLUBS, Rank.NINE);
+    Card five = new Card(Suit.DIAMONDS, Rank.FIVE);
+    hand.addCard(ten);
+    hand.addCard(nine);
+    hand.addCard(five);
+    assertTrue(hand.isBust());
+  }
+
+  @Test
+  public void testIsNotBust() {
+    // Test if the hand is not a bust (points do not exceed 21)
+    Card ten = new Card(Suit.HEARTS, Rank.TEN);
+    Card nine = new Card(Suit.CLUBS, Rank.NINE);
+    hand.addCard(ten);
+    hand.addCard(nine);
+    assertFalse(hand.isBust());
   }
 
   @Test
   public void testIsPair() {
-    Hand hand = new Hand();
-    hand.addCard(new Card(Suit.SPADES, Rank.TEN));
-    hand.addCard(new Card(Suit.HEARTS, Rank.TEN));
-    assertTrue("Hand should be a pair", hand.isPair());
+    // Test if the hand is a pair (two cards of the same rank)
+    Card ten1 = new Card(Suit.HEARTS, Rank.TEN);
+    Card ten2 = new Card(Suit.CLUBS, Rank.TEN);
+    hand.addCard(ten1);
+    hand.addCard(ten2);
+    assertTrue(hand.isPair());
+  }
+
+  @Test
+  public void testIsNotPair() {
+    // Test if the hand is not a pair (two cards of different ranks)
+    Card ten = new Card(Suit.HEARTS, Rank.TEN);
+    Card nine = new Card(Suit.CLUBS, Rank.NINE);
+    hand.addCard(ten);
+    hand.addCard(nine);
+    assertFalse(hand.isPair());
   }
 }
