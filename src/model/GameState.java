6bd51cf379d4414d505446dc8fdcb69325c7ed69
bathwaterpizza <cakto002@gmail.com>
2024-11-05 16:48:25 -0300

    New GameState record encapsulates arguments to the observer update methods

---
+++ src/model/GameState.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.List;
+
+// this record is used as an argument to pass the game state to game observers
+public record GameState(
+    List<Integer> dealerCards,
+    int dealerPoints,
+    List<Integer> playerCards,
+    List<Integer> playerSplitCards,
+    int playerPoints,
+    int playerSplitPoints,
+    boolean splitPlaying,
+    int balance,
+    int totalBet,
+    int bet,
+    int splitBet,
+    String message) {
+}
--- src/observer/IGameObserver.java
+++ src/observer/IGameObserver.java
@@ -1,6 +1,12 @@
 package observer;
 
+import model.GameState;
+
 // Interface for the class that will be observing
 public interface IGameObserver {
+  public void updateHand(IGameObservable observable, GameState state);
 
+  public void updateMoney(IGameObservable observable, GameState state);
+
+  public void updateRoundResult(IGameObservable observable, GameState state);
 }
