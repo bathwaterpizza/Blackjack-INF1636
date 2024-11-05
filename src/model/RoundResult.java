c93745485647b40b4f21c6f8cb9eb92445fedd22
bathwaterpizza <cakto002@gmail.com>
2024-11-05 16:53:12 -0300

    Add RoundResult enum to define the result messages in the dialog box, implementation pending

---
+++ src/model/RoundResult.java
@@ -0,0 +1,6 @@
+package model;
+
+// contains all the possible outcomes for a round, and their corresponding message
+public enum RoundResult {
+  // TODO: add the possible outcomes and a dialog message for each
+}
